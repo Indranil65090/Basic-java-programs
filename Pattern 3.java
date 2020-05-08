87eef599c21402e395ed99e3e81c3d8dcb2e5c02
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-09 00:09:07 +0530

    Create Pattern 3.java

---
+++ Pattern 3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Pattern 3
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the number of rows:");
+        int n=sc.nextInt();
+        int n1=n;
+        for(int r=0;r<n;r++)
+        {
+            if(n1>1)
+            {
+                for(int space=n1-1;space>=1;space--)
+                {
+                    System.out.print(" ");
+                }
+            }
+            for(int i=1;i<=(2*r+1);i++)
+            {
+                System.out.print(i);
+            }
+            n1--;
+            System.out.println();
+        }
+    }
+}
