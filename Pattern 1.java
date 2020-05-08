5da2daf1bdb43680435605eed083c97efb02e00b
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-08 23:47:18 +0530

    Create Pattern 1.java

---
+++ Pattern 1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Pattern_1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows:");
+        int n = sc.nextInt();
+        int n1=n;
+        for(int r=1;r<=n;r++)
+        {
+            if(r>1)
+            {
+                for(int space=1;space<=r-1;space++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            for(int i=n1;i>=1;i--)
+            {
+                System.out.print("*");
+            }
+            n1--;
+            System.out.println();
+        }
+    }
+}
+
+
+
+
