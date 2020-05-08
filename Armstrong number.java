af421e0a45321efe022d40583b91deff0e4105fd
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-08 23:23:09 +0530

    Create Armstrong number.java

---
+++ Armstrong number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Armstrongnumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number from user:");
+        int n=sc.nextInt();
+        int result=0,temp;
+        int a=n;
+        while(a!=0)
+        {
+            temp=a%10;
+            result=result*10+temp;
+            a/=10;
+        }
+        if(result==n)
+        {
+            System.out.println("This number "+n+" is pallindrome");
+        }
+        else
+        {
+            System.out.println("This number "+n+" is not pallindrome");
+        }
+    }
+}
