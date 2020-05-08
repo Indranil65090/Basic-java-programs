47be6d5f578957a2af67e00219fe4f8a5560c928
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-09 00:12:08 +0530

    Update and rename Pallindromenumber.java to Palindrome number.java
    Please check that the file name is same as that of the class name(only for beginners)

--- Palindrome number.java
+++ Palindrome number.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
-public class Pallindromenumber {
-    public static void main(String[] args) {
+public class Palindrome number 
+{
+    public static void main(String[] args) 
+    {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number from user:");
         int n=sc.nextInt();
